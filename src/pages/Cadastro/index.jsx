6ae663da587abea759713b67cce269da414fa210
Marcelo <marceloo.dev@gmail.com>
2025-03-17 11:44:34 -0300

    update login and registration pages with Lottie animations and improve layout

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -4,7 +4,6 @@ import { useRef } from "react";
 import api from "../../services/api";
 import "./Cadastro.css";
 
-
 const Cadastro = () => {
   const nameRef = useRef();
   const emailRef = useRef();
@@ -20,35 +19,37 @@ const Cadastro = () => {
       });
       alert("Usuário cadastrado com sucesso!");
     } catch (error) {
-      console.log(err);
+      console.log(error);
       alert("Erro ao cadastrar usuário!");
     }
   };
 
   return (
     <div className="container">
-      <h2 className="title2">Cadastro</h2>
-      <form className="form" onSubmit={handleSubmit}>
-        <input ref={nameRef} className="input" type="text" placeholder="Nome" />
-        <input
-          ref={emailRef}
-          className="input"
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          ref={passwordRef}
-          className="input"
-          type="password"
-          placeholder="Senha"
-        />
-        <button className="button" type="submit">
-          Cadastrar-se
-        </button>
-      </form>
-      <Link className="link" to="/login">
-        Já tem uma conta? Fazer login!
-      </Link>
+      <div className="content">
+        {/* Animação do Lottie via CDN */}
+        <div className="animation">
+          <iframe 
+            src="https://lottie.host/embed/c4eae121-f563-4516-8959-cfdf78c25d5f/HOhpwqgmOT.lottie" 
+            title="Lottie Animation"
+            frameBorder="0"
+            width="100%"
+            height="100%"
+          />
+        </div>
+
+        {/* Formulário de Cadastro */}
+        <div className="form-section">
+          <h2 className="title2">Cadastro</h2>
+          <form className="form" onSubmit={handleSubmit}>
+            <input ref={nameRef} className="input" type="text" placeholder="Nome" />
+            <input ref={emailRef} className="input" type="email" placeholder="Email" />
+            <input ref={passwordRef} className="input" type="password" placeholder="Senha" />
+            <button className="button" type="submit">Cadastrar-se</button>
+          </form>
+          <Link className="link" to="/login">Já tem uma conta? Fazer login!</Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import api from "../../services/api";
+import "./Login.css";
 
 const Login = () => {
   const emailRef = useRef();
@@ -20,34 +21,36 @@ const Login = () => {
 
       navigate("/listar-usuarios");
     } catch (error) {
-      console.log(err);
+      console.log(error);
       alert("Email ou senha incorretos!");
     }
   };
 
   return (
     <div className="container">
-      <h2 className="title">Cadastro</h2>
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          ref={emailRef}
-          className="input"
-          type="email"
-          placeholder="Email"
-        />
-        <input
-          ref={passwordRef}
-          className="input"
-          type="password"
-          placeholder="Senha"
-        />
-        <button className="button" type="submit">
-          Login
-        </button>
-      </form>
-      <Link className="link" to="/">
-        Não tem uma conta? Cadastre-se!
-      </Link>
+      <div className="content">
+        {/* Animação do Lottie */}
+        <div className="animation">
+          <iframe 
+            src="https://lottie.host/embed/8b0cdcc2-2d53-4e00-88e5-61986641fbce/HfMWWXNsk2.lottie" 
+            title="Lottie Animation"
+            frameBorder="0"
+            width="100%"
+            height="100%"
+          />
+        </div>
+
+        {/* Formulário de Login */}
+        <div className="form-section">
+          <h2 className="title2">Login</h2>
+          <form className="form" onSubmit={handleSubmit}>
+            <input ref={emailRef} className="input" type="email" placeholder="Email" />
+            <input ref={passwordRef} className="input" type="password" placeholder="Senha" />
+            <button className="button" type="submit">Login</button>
+          </form>
+          <Link className="link" to="/">Não tem uma conta? Cadastre-se!</Link>
+        </div>
+      </div>
     </div>
   );
 };
